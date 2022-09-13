0ed230b0a22974adc9be95c649515bba72ab31de
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-13 11:57:28 -0700

    Added HTML and some Java to get the button clicks to display in Player Choice section

--- app.js
+++ app.js
@@ -6,7 +6,20 @@
 function loadPage() {}
 
 /* Components */
+const playerChoiceDisplay = document.getElementById('player-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('result');
 
+const possibleChoices = document.querySelectorAll('button');
+
+let userChoice;
+
+possibleChoices.forEach((possibleChoice) =>
+    possibleChoice.addEventListener('click', (e) => {
+        userChoice = e.target.id;
+        playerChoiceDisplay.innerHTML = userChoice;
+    })
+);
 /* Component */
 // get DOM
 // display
